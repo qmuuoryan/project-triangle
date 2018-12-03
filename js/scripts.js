10ae026de9a1aa3f58a80a4a76a08d978b839070
papi <ryanmuuo91@gmail.com>
2018-12-03 07:04:27 +0300

    js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
+
 function triangle(){
-var A = parseInt(Document.getElementById("input sideA:"));
-var B = parseInt(Document.getElementById("input sideB:"));
-var C = parseInt(Document.getElementById("input sideC:"));
+var A = parseInt(prompt("input sideA:"));
+var B = parseInt(prompt("input sideB:"));
+var C = parseInt(prompt("input sideC:"));
 
 if(A===B && A===C && B===C){
   alert("equilateral");
-}else if(A===B || A===C || B===C); {
+}else if(A===B || A===C || B===C) {
   alert("isoceles");
-}else if((A+B)>=C && (B+C)>=A || (A+C)>=B); {
+}else if((A+B)>=C && (B+C)>=A || (A+C)>=B) {
   alert("Scalene");
-}else if((A+B)<= C || (A+C)<=B || (B+C)<=A); {
+}else if((A+B)<= C || (A+C)<=B || (B+C)<=A) {
   alert("Not A Triangle");
 }
 }
