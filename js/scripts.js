b3fe480650e7455ef960e2a6b34e38b7055519b5
papi <ryanmuuo91@gmail.com>
2018-12-04 15:23:56 +0300

    add the changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 
 function triangle(){
-var a = parseInt(document.getElementById("side1").value);
-var b = parseInt(document.getElementById("side2").value);
-var c = parseInt(document.getElementById("side3").value);
+var a = parseInt(document.getElementById("sidea").value);
+var b = parseInt(document.getElementById("sideb").value);
+var c = parseInt(document.getElementById("sidec").value);
 
 if(a===c && a===b && b===c){
   alert("equilateral");
@@ -10,7 +10,7 @@ if(a===c && a===b && b===c){
   alert("isoceles");
 }else if((a+b)>=c && (b+c)>=a || (a+c)>=b) {
   alert("Scalene");
-}else {
+}else if((a+b)<= c || (a+c)<=b || (b+c)<=a){
   alert("Not A Triangle");
 }
 }
